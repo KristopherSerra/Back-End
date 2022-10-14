82dcc0a36645fdc7cd89db189bbb23ff9dc2dd5c
Kristopher Serra <cdserra@alumnos.unsada.edu.ar>
2022-10-14 14:44:41 -0300

    Changes to be committed: 	new file:   Main.py

---
+++ Main.py
@@ -0,0 +1,12 @@
+'''
+3 - Un script Python que se encargará de crear y suministrar de números telefónicos a la
+base de datos creada en el punto anterior. Para esto, se deberá procesar un archivo
+Excel que contiene todos los rangos de numeración telefónica del país.
+4 - Un script Python, que permita generar hasta 10 archivos de tipo “Microsoft Access”
+donde cada uno cuente con 7.000 números telefónicos distintos (según las localidades
+seleccionadas para una campaña publicitaria) cada una. Estos archivos se generan
+tomando información de la base de datos creada en el punto 2.
+5 - Un script Python que sea capaz de crear una planilla de cálculo a modo de reporte con
+los datos de una campaña publicitaria finalizada dada. Este script, además debe enviar
+por mail el reporte creado al cliente en cuestión.
+'''
