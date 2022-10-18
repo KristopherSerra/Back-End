0c6e7bf7abba5f1e291b23ea4026be52a41770f5
KristopherSerra <105523175+KristopherSerra@users.noreply.github.com>
2022-10-18 18:09:26 -0300

    Update 5.py

--- Back-End/5/5.py
+++ Back-End/5/5.py
@@ -1,1 +1,12 @@
+'''
+5 - Un script Python que sea capaz de crear una planilla de cálculo a modo de reporte con
+los datos de una campaña publicitaria finalizada dada. Este script, además debe enviar
+por mail el reporte creado al cliente en cuestión.
+'''
 
+import pandas as panda
+import openpyxl
+
+informes = panda.read_excel("C:\\Users\Kristopher\Desktop\De Todo\\Universidad\LdL\BackEnd\Laboratorio-Full-\Back-End\Informes.xlsx")
+
+tabla_pivote = informes.pivot_table(index='Cant. Total', columns='Dia',values='Total',aggfunc='sum')
