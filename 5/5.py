47580155288788d57af32295c7ff032623c0d329
Kristopher Serra <cdserra@alumnos.unsada.edu.ar>
2022-10-18 21:07:49 -0300

    modified:   5/5.py 	new file:   5/BDE.xlsx 	new file:   5/InformeAutomatico.xlsx

--- 5/5.py
+++ 5/5.py
@@ -4,9 +4,9 @@ los datos de una campaña publicitaria finalizada dada. Este script, además deb
 por mail el reporte creado al cliente en cuestión.
 '''
 
-import pandas as panda
+import pandas as pd
 import openpyxl
 
-informes = panda.read_excel("C:\\Users\Kristopher\Desktop\De Todo\\Universidad\LdL\BackEnd\Laboratorio-Full-\Back-End\Informes.xlsx")
-
-tabla_pivote = informes.pivot_table(index='Cant. Total', columns='Dia',values='Total',aggfunc='sum')
+informes = pd.read_excel("5\BDE.xlsx")
+tabla_pivote = informes.pivot_table(index="LOCALIDAD",columns='BLOQUE', values='INDICATIVO', aggfunc='sum').round(0)
+tabla_pivote.to_excel('5\InformeAutomatico.xlsx', startrow=4,starcolumn=2,sheet_name='Octubre 2022')
