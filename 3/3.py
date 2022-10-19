d0985a35add4a2fc245ee3c2ab643831c6898b4d
Matias Bue <matias.bue@outlook.com>
2022-10-19 20:26:46 -0300

    Funcion añadida para calcular cuantos digitos faltan

--- 3/3.py
+++ 3/3.py
@@ -1,14 +1,50 @@
-import openpyxl
+from pyclbr import Function
+import pandas as pd
 
-book = openpyxl.load_workbook('rangos.xlsx', data_only=True)
-sheet = book["rangos"]
-indicativo = []
+# ------------------------------- FUNCIONES -------------------------------
+# Une el cod. area y el bloque
+def get_range(i):
+    cod = str(location.iloc[i].INDICATIVO);
+    block = str(location.iloc[i].BLOQUE)
+    conc = cod + block
+    return int(conc)
 
-localidad = input("Ingrese localidad:")
+
+# ---------------------------------- MAIN ----------------------------------
+
+book = pd.read_csv('rangos.csv')
+
+# dataFrame con la info de la localidad pedida
+location = book[book['LOCALIDAD'] == input('Ingrese localidad: ').upper()]
+
+# Contiene el conjunto del codigo de area + el bloque
+rangArray = [] 
+
+for i in range(len(location)):
+    rangArray.append(get_range(i))
 
 
-for rows in sheet.iter_cols(min_col = 1, max_col = 1, min_row = 2, max_row = 115):
-    for row in rows:
-        if (row.value == localidad):
-            print(row.value)
-        
+# Almacena cuantos digitos faltan para completar el numero
+faltantes = [] 
+for i in range(len(rangArray)):
+    faltantes.append(10 - len(str(rangArray[i])))
+
+# Crear el numero
+numeros = []
+
+
+
+
+# print para test
+for i in range(len(location)):
+    print(rangArray[i])
+
+
+# Importa el codigo de area y bloque en archivos separados (no requerido por ahora)
+
+# codArea = location['INDICATIVO']
+# bloque = location['BLOQUE']
+# codArea.to_csv('codArea.csv', encoding='utf-8', index=False)
+# bloque.to_csv('bloque.csv', encoding='utf-8', index=False)
+
+
